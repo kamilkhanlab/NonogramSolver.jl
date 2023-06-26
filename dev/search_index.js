02f5dcc1a7082d64467819964ba3fd20b60cf82f
Documenter.jl <documenter@juliadocs.github.io>
2023-06-26 01:36:33 +0000

    build based on 1e35be6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NonogramSolver","category":"page"},{"location":"#NonogramSolver","page":"Home","title":"NonogramSolver","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NonogramSolver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NonogramSolver]","category":"page"}]
+}
